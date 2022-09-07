e9eec4d818aae616dbbb6b3582948c5a1e44c574
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-09-07 12:48:51 +0200

    Add files via upload

---
+++ Cursist.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace cursistToevoegen
+{
+    public class Cursist
+    {
+        public Cursist(string voornaam, string achternaam)
+        {
+            Naam = voornaam;
+            Achternaam = achternaam;
+        }
+
+        public string Naam { get; set; }
+        public string Achternaam { get; set; }
+
+        public string Fullname { get => $"{Naam} {Achternaam}"; }
+    }
+}
